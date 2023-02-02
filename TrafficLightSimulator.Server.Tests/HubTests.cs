2357f856f394537f38cea1a60fd2b7e378e039e4
Bo Tan <bo.tan@chemistwarehouse.com.au>
2023-02-02 13:16:33 +1100

    add test

---
+++ TrafficLightSimulator.Server.Tests/HubTests.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.AspNetCore.SignalR;
+using Moq;
+using SignalR_UnitTestingSupportCommon.Hubs;
+using TrafficLightSimulator.Server.Hubs;
+using TrafficLightSimulator.Server.Models;
+using TrafficLightSimulator.Server.Services;
+
+namespace TrafficLightSimulator.Server.Tests
+{
+    public class HubTests
+    {
+        private readonly Mock<ITrafficLightService> _mockService;
+
+        public HubTests()
+        {
+            _mockService = new Mock<ITrafficLightService>();
+        }
+
+        [Fact]
+        public void TestHub_ConnectionId_ExpectedResult()
+        {
+            var support = new HubUnitTestsSupport();
+            support.SetUp();
+            var hub = new TrafficLightHub(_mockService.Object);
+            support.AssignToHubRequiredProperties(hub);
+
+            var connectionId = hub.Context.ConnectionId;
+
+            Assert.True(!string.IsNullOrWhiteSpace(connectionId));
+        }
+    }
+}
